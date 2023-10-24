812dbe19a6338022821d7e14f78e1460e46af561
Vanilson23 <127863090+Vanilson23@users.noreply.github.com>
2023-10-24 20:23:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ function tocaSomPom() {
 document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('tecla_pom').onclick= tocaSomPom;
+const listaDeBotoes = document.querySelectorAll;
 
+let contador = 0;
 
-
+white (contador < 9) {
+   listaDeBOtoes[contador].onclick = tocaSomPom;
+   contador = contador + 1;
+}
